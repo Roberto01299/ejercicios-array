800972806ff17e19dff2ead80f20d5e0cd7fde22
Roberto01299 <78993100+Roberto01299@users.noreply.github.com>
2021-03-02 09:13:16 -1200

    Add files via upload

---
+++ Program Ejercicio7.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ejercicio7
+{
+    class Program
+    {
+        static void Main(string[] args)
+//Un programa que te pida tu nombre y lo muestre en pantalla separando cada letra de la siguiente con un espacio. 
+//Por ejemplo, si tu nombre es "Juan", debería aparecer en pantalla "J u a n".
+        {
+          string nombre;
+
+              Console.Write("Escribe tu nombre: ");
+              nombre = Console.ReadLine();
+
+              foreach (var sub in nombre)
+              {
+                  Console.Write($" {sub}");
+              }
+
+              Console.ReadKey();
+              
+        }
+    }
+}
+
